c5700229f859df43ed7ef24d24f62b2470067fcf
G Sai Dasaradharam <sec19ic010@sairamtap.edu.in>
2020-10-17 17:25:49 +0530

    directory

--- models/directory/directoryAttributesGroupsModel.js
+++ models/directory/directoryAttributesGroupsModel.js
@@ -1,26 +1,35 @@
-const directoryAttributeGroups = {
+/* DEPENDENCIES */
+const mongoose = require('mongoose');
+
+/* SCHEMA CONSTRUCTOR */
+const { Schema } = mongoose;
+
+/* SCHEMA */
+const directoryAttributeGroupsSchema = new Schema({
     _id:{type:mongoose.ObjectId},
     name:{type:String},   
-    attributes:{
-        [
-            {
-                _id:{type:mongoose.ObjectId},
-                name:{type:String},   
-                type:{type:String},
-                Description:{type:String},
-                notes:{type:String},
-                status:{type:String},
-                updatedBy:{type:mongoose.ObjectId},
-                updatedAt:{type:Date},
-                createdBy:{type:mongoose.ObjectId},
-                createdAt:{type:Date}        
-            }
-        ]
-    } ,
+    attributes:[
+        {
+            _id:{type:mongoose.ObjectId},
+            name:{type:String},   
+            type:{type:String},
+            Description:{type:String},
+            notes:{type:String},
+            status:{type:String},
+            updatedBy:{type:mongoose.ObjectId},                updatedAt:{type:Date},
+            createdBy:{type:mongoose.ObjectId},
+            createdAt:{type:Date}        
+        }
+    ],
     status:{type:String},
     description:{type:String},   
     updatedBy:{type:mongoose.ObjectId},
     updatedAt:{type:Date},
     createdBy:{type:mongoose.ObjectId},
     createdAt:{type:Date} 
-}
+});
+/* MODEL */
+const DirectoryAttributeGroups = mongoose.model('directoryAttributeGroups', directoryAttributeGroupsSchema);
+
+/* EXPORT */
+module.exports = DirectoryAttributeGroups;
