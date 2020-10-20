70df4af438f72a75ee634be9a1dfa1b558266b3d
G Sai Dasaradharam <sec19ic010@sairamtap.edu.in>
2020-10-20 10:01:40 +0530

    precision_ag

--- models/precisionAg/farmRegionsModel.js
+++ models/precisionAg/farmRegionsModel.js
@@ -1,6 +1,19 @@
-const farmRegions={
+/* DEPENDENCIES */
+const mongoose = require('mongoose');
+
+/* SCHEMA CONSTRUCTOR */
+const { Schema } = mongoose;
+
+/* SCHEMA */
+const farmRegionsSchema = new Schema({
     _id:{type:mongoose.ObjectId},
     _farmId:{type:mongoose.ObjectId},
     _zoneId:{type:mongoose.ObjectId},
     consultant:[]
-}
+});
+
+/* MODEL */
+const FarmRegions = mongoose.model('farmRegions', farmRegionsSchema);
+
+/* EXPORT */
+module.exports = FarmRegions;
