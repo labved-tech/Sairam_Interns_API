a0cc38141cc42df3b4a7f39de9764ec09fef6494
vignesh bs <sec19ic019@sairamtap.edu.in>
2020-10-22 19:17:08 +0530

    routes

---
+++ routes/ecommerce/address.js
@@ -0,0 +1,9 @@
+const address = {
+    _id:{type:mongoose.ObjectId},
+    Address1:{type:String},
+    street:{type:String},
+    City:{type:String},
+    State:{type:String},
+    country:{type:String},
+    postalcode:{type:Number}
+}
