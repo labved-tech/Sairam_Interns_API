aa908467fe8e5079e8d9d0a35f1f5aa96bcbb19f
vignesh bs <sec19ic019@sairamtap.edu.in>
2020-10-22 19:50:11 +0530

    ratings

--- routes/ratings/ratingAttributeRoutes.js
+++ routes/ratings/ratingAttributeRoutes.js
@@ -1,12 +1,27 @@
-const ratingAttribute = {
-    _id:{type:mongoose.ObjectId},
-    Name:{type:String}, 
-    type:{type:String},
-    Description:{type:String}, 
-    Notes:{type:String},
-    status:{type:String},    
-    updatedAt:{type:Date}, 
-    updatedBy:{type:mongoose.ObjectId},
-    createdAt:{type:Date},
-    createdBy:{type:mongoose.ObjectId}    
-}
+/* DEPENDENCIES */
+const express = require('express');
+
+/* MIDDLEWARE */
+const router = express.Router();
+const ratingAttributeController = require('./../../controllers/ratings/ratingAttributeController');
+
+/* GLOBAL MIDDLEWARE USAGE*/
+router.use((req, res, next) => {
+  console.log('We are in ratingAttributeRoutes');
+  next();
+});
+
+router.param('id', ratingAttributeController.checkID);
+
+/* ROUTES */
+router
+  .route('/')
+  .get(ratingAttributeController.getAllRatingAttribute)
+  .post(ratingAttributeController.createRatingAttribute);
+router
+  .route('/:id')
+  .get(ratingAttributeController.getRatingAttribute)
+  .patch(ratingAttributeController.updateRatingAttribute)
+  .delete(ratingAttributeController.deleteRatingAttribute);
+
+module.exports = router;
