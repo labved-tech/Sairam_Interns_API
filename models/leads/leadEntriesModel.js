bd3cd6e4cddd1ade26ec3f024a74826b4ff7432b
labved <bharath.viswanathan@labved.com>
2020-11-05 18:59:53 +0530

    updated leadEntries

--- models/leads/leadEntriesModel.js
+++ models/leads/leadEntriesModel.js
@@ -5,59 +5,60 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 /* SCHEMA */
-const leadEntriesSchema = new Schema({
-  name: { type: String },
-  title: { type: String },
-  _categoryId: { type: mongoose.ObjectId },
-  company: { type: String },
-  description: { type: String },
-  email: { type: String },
-  website: { type: String },
-  address: {
-    _id: { type: mongoose.ObjectId },
-    address1: { type: String },
-    street: { type: String },
-    city: { type: String },
-    state: { type: String },
-    country: { type: String },
-    postalCode: { type: String },
-  },
-  contactInformation: [
-    {
+const leadEntriesSchema = new Schema(
+  {
+    name: { type: String },
+    title: { type: String },
+    _categoryId: { type: mongoose.ObjectId },
+    company: { type: String },
+    description: { type: String },
+    email: { type: String },
+    website: { type: String },
+    address: {
       _id: { type: mongoose.ObjectId },
-      item: { type: String },
-      type: { type: String },
-      isPublic: { type: Boolean },
-      createdBy: { type: mongoose.ObjectId, required: 1  },
-      updatedBy: { type: mongoose.ObjectId, required: 1  },
+      address1: { type: String },
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      country: { type: String },
+      postalCode: { type: String },
     },
-      { timestamps: true }
-  ],
-  assignedTo: { type: String },
-  source: {
-    _id: { type: mongoose.ObjectId },
-    name: { type: String },
+    contactInformation: [
+      {
+        item: { type: String },
+        type: { type: String },
+        isPublic: { type: Boolean },
+        createdBy: { type: mongoose.ObjectId, required: 1 },
+        updatedBy: { type: mongoose.ObjectId, required: 1 },
+        createdAt: { type: Date, required: 1 },
+        updatedAt: { type: Date, required: 1 },
+      },
+    ],
+    assignedTo: { type: String },
+    source: {
+      _id: { type: mongoose.ObjectId },
+      name: { type: String },
+    },
+    _sourceId: { type: mongoose.ObjectId },
+    leadStatus: {
+      _id: { type: mongoose.ObjectId },
+      name: { type: String },
+      statusOrder: { type: String },
+      colour: { type: String },
+      isDefault: { type: String },
+      notes: { type: String },
+    },
+    lastContact: { type: Date },
+    priority: { type: Number },
+    dateConverted: { type: Date },
+    lost: { type: Boolean },
+    lastLeadStatus: { type: String },
+    lastStatusChange: { type: Date },
+    status: { type: String },
+    createdBy: { type: mongoose.ObjectId, required: 1 },
+    updatedBy: { type: mongoose.ObjectId, required: 1 },
   },
-  _sourceId: { type: mongoose.ObjectId },
-  leadStatus: {
-    _id: { type: mongoose.ObjectId },
-    name: { type: String },
-    statusOrder: { type: String },
-    colour: { type: String },
-    isDefault: { type: String },
-    notes: { type: String },
-  },
-  lastContact: { type: Date },
-  priority: { type: Number },
-  dateConverted: { type: Date },
-  lost: { type: Boolean },
-  lastLeadStatus: { type: String },
-  lastStatusChange: { type: Date },
-  status: { type: String },
-  createdBy: { type: mongoose.ObjectId, required: 1 },
-  updatedBy: { type: mongoose.ObjectId, required: 1 },
-},
-{ timestamps: true }
+  { timestamps: true }
 );
 
 /* MODEL */
