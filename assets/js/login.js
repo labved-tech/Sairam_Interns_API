0ea51768adfa0309c92ef97b8ca8cf7d733880b5
labved <bharath.viswanathan@labved.com>
2020-11-06 20:47:58 +0530

    update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-const login = (email, password) => {
-  alert(email, password);
+const login = async (email, password) => {
+  console.log(email, password);
+
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
+  } catch (err) {
+    console.log(err.response.data);
+  }
 };
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('[name="username"]').value;
-  const password = document.getElementById('[name="password"]').value;
-  console.log(email);
-  login({ email, password });
-});
+document
+  .querySelector('kt_login_singin_form')
+  .addEventListener('kt_login_singin_form_submit_button', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('[name="email"]').value;
+    const password = document.getElementById('[name="password"]').value;
+
+    console.log(email, password);
+
+    login(email, password);
+  });
