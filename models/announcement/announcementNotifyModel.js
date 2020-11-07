ef9f25f6d3d5b4207636992d197d2b532cb0f1a2
Gowri S <70696665+GowriSaravanan@users.noreply.github.com>
2020-11-07 12:51:55 +0530

    Announcement & Newsletter

--- models/announcement/announcementNotifyModel.js
+++ models/announcement/announcementNotifyModel.js
@@ -20,8 +20,8 @@ const announcementNotifySchema = new Schema(
         sentAt: { type: Date },
       },
     ],
-    createdBy: { type: mongoose.ObjectId, required: true },
-    updatedBy: { type: mongoose.ObjectId, required: true },
+    createdBy: { type: mongoose.ObjectId  },
+    updatedBy: { type: mongoose.ObjectId },
   },
   { timestamps: true }
 );
