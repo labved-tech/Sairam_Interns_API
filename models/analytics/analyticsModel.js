7b9e8d2ec40372bb92a39ac1f307ab817d53357f
labved <bharath.viswanathan@labved.com>
2020-11-09 16:39:31 +0530

    updated postman analytics

--- models/analytics/analyticsModel.js
+++ models/analytics/analyticsModel.js
@@ -5,15 +5,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 /* SCHEMA */
-const analyticsSchema = new Schema({
-  name: { type: String },
-  state: { type: Object },
-  _ownerid: { type: mongoose.ObjectId },
-  _reportid: { type: mongoose.ObjectId },
-  createdBy: { type: mongoose.ObjectId},
-  updatedBy: { type: mongoose.ObjectId}
-},
-{ timestamps: true }
+const analyticsSchema = new Schema(
+  {
+    name: { type: String },
+    state: { type: Object },
+    _ownerid: { type: mongoose.ObjectId },
+    _reportid: { type: mongoose.ObjectId },
+    createdBy: { type: mongoose.ObjectId },
+    updatedBy: { type: mongoose.ObjectId },
+  },
+  { timestamps: true }
 );
 
 /* MODEL */
