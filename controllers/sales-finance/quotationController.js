ab45ace141c002b56c2b69e6616a5934f466fa44
vignesh bs <sec19ic019@sairamtap.edu.in>
2020-11-10 19:10:28 +0530

    updates

--- controllers/sales-finance/quotationController.js
+++ controllers/sales-finance/quotationController.js
@@ -10,41 +10,41 @@ const Quotation = require('./../../models/sales-finance/quotationModel');
 
 /* CONTROLLERS */
 exports.checkID = (req, res, next, val) => {
-    const { id } = req.params.id;
-    console.log(`ID is ${id}`);
-    next();
-  };
-  
-  exports.getAllQuotation = catchAsync(async (req, res, next) => {
-    console.log('Getting All Quotation');
-    const quotation = await Quotation.find().then();
-  
-    res.status(200).json({
-      status: 'sucess',
-      message: 'Got All Quotation',
-      results: quotation.length,
-      data: {
-        quotations:"message",
-      },
-    });
-    next();
+  const { id } = req.params.id;
+  console.log(`ID is ${id}`);
+  next();
+};
+
+exports.getAllQuotation = catchAsync(async (req, res, next) => {
+  console.log('Getting All Quotation');
+  const quotation = await Quotation.find().then();
+
+  res.status(200).json({
+    status: 'sucess',
+    message: 'Got All Quotation',
+    results: quotation.length,
+    data: {
+      quotation,
+    },
   });
-  
-  exports.getQuotation = catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    console.log(`Getting Quotation for Id ${id}`);
-    const quotation = await Quotation.findById(id).then();
-    res.status(200).json({
-      status: 'sucess',
-      message: `Got Quotation Id=${id}`,
-      Data: { quotation },
-    }); 
-    next();
+  next();
+});
+
+exports.getQuotation = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  console.log(`Getting Quotation for Id ${id}`);
+  const quotation = await Quotation.findById(id).then();
+  res.status(200).json({
+    status: 'sucess',
+    message: `Got Quotation Id=${id}`,
+    Data: { quotation },
   });
-  
-  exports.createQuotation = catchAsync(async (req, res, next) => {
-    console.log('Creating Quotation');
-    // parse through models
+  next();
+});
+
+exports.createQuotation = catchAsync(async (req, res, next) => {
+  console.log('Creating Quotation');
+  // parse through models
   const doc = new Quotation(req.body);
   console.log(doc);
 
@@ -64,56 +64,55 @@ exports.checkID = (req, res, next, val) => {
   // check the doc before doing database operation
   //console.log(doc);
 
-    const quotation = await Quotation.create(doc).then();
-  
-    res.status(201).json({
-      status: 'sucess',
-      message: 'Created Quotation',
-      data: { quotation },
-    }); 
-    next();
-  });
-  
-  exports.updateQuotation = catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    console.log(`Updating Quotation Id ${id}`);
-    const { body } = req;
-   
-   // parse through models
-   const quotationToUpdate = new Quotation(body);
-   console.log(body);
-   const doc = quotationToUpdate.toObject();
-   delete doc._id;
- 
- 
-   // update timestamps & Id's
-   doc.updatedBy = '5f990bb3c727e952a076f3b7'; // user id
-   doc.updatedAt;
- 
-   // check the doc before doing database operation
-   //console.log(doc);
-    const quotation = await Quotation.findByIdAndUpdate(id, req.body, {
-      new: true,
-    }).then();
+  const quotation = await Quotation.create(doc).then();
 
-    res.status(201).json({
-      status: 'sucess',
-      message: `Updated Quotation Id=${id}`,
-      data: { Quotation },
-    });  
-    next();
+  res.status(201).json({
+    status: 'sucess',
+    message: 'Created Quotation',
+    data: { quotation },
   });
-  
-  exports.deleteQuotation = catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    console.log(`Deleting Quotation Id ${id}`);
-    const quotation = await Quotation.findByIdAndDelete(id).then();
-  
-    res.status(200).json({
-      status: 'sucess',
-      message: `Deleted Quotation Id=${id}`,
-      data: { quotation },
-    });
-    next();
+  next();
+});
+
+exports.updateQuotation = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  console.log(`Updating Quotation Id ${id}`);
+  const { body } = req;
+
+  // parse through models
+  const quotationToUpdate = new Quotation(body);
+  console.log(body);
+  const doc = quotationToUpdate.toObject();
+  delete doc._id;
+
+
+  // update timestamps & Id's
+  doc.updatedBy = '5f990bb3c727e952a076f3b7'; // user id
+  doc.updatedAt;
+
+  // check the doc before doing database operation
+  //console.log(doc);
+  const quotation = await Quotation.findByIdAndUpdate(id, req.body, {
+    new: true,
+  }).then();
+
+  res.status(201).json({
+    status: 'sucess',
+    message: `Updated Quotation Id=${id}`,
+    data: { quotation },
   });
-  
+  next();
+});
+
+exports.deleteQuotation = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  console.log(`Deleting Quotation Id ${id}`);
+  const quotation = await Quotation.findByIdAndDelete(id).then();
+
+  res.status(200).json({
+    status: 'sucess',
+    message: `Deleted Quotation Id=${id}`,
+    data: { quotation },
+  });
+  next();
+});
