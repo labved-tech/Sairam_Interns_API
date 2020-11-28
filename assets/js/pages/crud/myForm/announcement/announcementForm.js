7877bf70bf8ce770e77442782298c9b76aa906d0
Gowri S <70696665+GowriSaravanan@users.noreply.github.com>
2020-11-28 19:08:36 +0530

    update newsletterMessage

--- assets/js/pages/crud/myForm/announcement/announcementForm.js
+++ assets/js/pages/crud/myForm/announcement/announcementForm.js
@@ -3,187 +3,187 @@
 
 /* Class definition */
 const AnnouncementCRUD = (function () {
-    const _buttonSpinnerClasses = 'spinner spinner-right spinner-white pr-15';
+  const _buttonSpinnerClasses = 'spinner spinner-right spinner-white pr-15';
 
-     /*   Private functions */
-     const _createAnnouncementEntriesForm = function () {
+  /*   Private functions */
+  const _createAnnouncementEntriesForm = function () {
 
-        // Getting Document related information
-        const createAnnouncementEntriesForm = KTUtil.getById('createAnnouncementEntriesForm');
-        const formSubmitButton = KTUtil.getById('announcementEntriesFormSubmit');
-        const aeTitle = KTUtil.getById('aeTitle');
-        const aeFrom = KTUtil.getById('aeFrom');
-        const aePriority = KTUtil.getById('aePriority');
-        const aeIsEmailReq = KTUtil.getById('aeIsEmailReq');
-        const aeExpires = KTUtil.getById('aeExpires');
-        const aeMessage = KTUtil.getById('aeMessage');
+    // Getting Document related information
+    const createAnnouncementEntriesForm = KTUtil.getById('createAnnouncementEntriesForm');
+    const formSubmitButton = KTUtil.getById('announcementEntriesFormSubmit');
+    const aeTitle = KTUtil.getById('aeTitle');
+    const aeFrom = KTUtil.getById('aeFrom');
+    const aePriority = KTUtil.getById('aePriority');
+    const aeIsEmailReq = KTUtil.getById('aeIsEmailReq');
+    const aeExpires = KTUtil.getById('aeExpires');
+    const aeMessage = KTUtil.getById('aeMessage');
 
-         // Initializing 
-         $(aeMessage).summernote({
-            height: 400,
-            tabsize: 2
-         });
-         
-         $(aeExpires).select2({
-          placeholder: "Select"
-          });
+    // Initializing 
+    $(aeMessage).summernote({
+      height: 400,
+      tabsize: 2
+    });
 
-        // Return Form
-         if (!createAnnouncementEntriesForm) {
-            return;
-          }
-      
-          // Validation
-          FormValidation.formValidation(createAnnouncementEntriesForm, {
-             fields: {
-              aeTitle: {
-                validators: {
-                  notEmpty: {
-                    message: 'This is field is required',
-                  },
-                },
-              },
-              aeFrom: {
-                validators: {
-                  notEmpty: {
-                    message: 'This is field is required',
-                  },
-                },
-              },
-              aePriority: {
-                validators: {
-                  notEmpty: {
-                    message: 'This is field is required',
-                  },
-                },
-              },
-/*               aeMessage: {
-                validators: {
-                  notEmpty: {
-                    message: 'This is field is required',
-                  },
-                },
-              }, */
-  
-              
-            }, 
-      
-            plugins: {
-              //Learn more: https://formvalidation.io/guide/plugins
-              trigger: new FormValidation.plugins.Trigger(),
-              // Bootstrap Framework Integration
-              bootstrap: new FormValidation.plugins.Bootstrap(),
-              // Validate fields when clicking the Submit button
-              submitButton: new FormValidation.plugins.SubmitButton(),
-              // Submit the form when all fields are valid
-              //defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+    $(aeExpires).select2({
+      placeholder: "Select"
+    });
+
+    // Return Form
+    if (!createAnnouncementEntriesForm) {
+      return;
+    }
+
+    // Validation
+    FormValidation.formValidation(createAnnouncementEntriesForm, {
+      fields: {
+        aeTitle: {
+          validators: {
+            notEmpty: {
+              message: 'This is field is required',
             },
-          })
-            .on('core.form.valid', function () {
-            // Show loading state on button
-              KTUtil.btnWait(formSubmitButton, _buttonSpinnerClasses, 'Please wait');
-              console.log(`Value:${aeTitle.value}`);
-      
-      
-            // Accessing Restful API
-            axios({
-              method: 'post',
-              url: `${HOST_URL}/api/v1/announcement/entries`,
-              data: {
-                title: aeTitle.value,
-                //message: aeMessage.value,
-                from: aeFrom.value,
-                isEmailReq: true,   //if(aeIsEmailReq.value=== 'on') return true
-                priority: (aePriority.value) * 1,
-                expires: aeExpires.value,
-              },
-      
-            }).then(function (res) {
-              // Return valid JSON
-              // Release button
-              KTUtil.btnRelease(formSubmitButton);
-              console.log(res);
-              
-              // SWAL EXAMPLE
-              /*         if (res.data.status == 'success') {
+          },
+        },
+        aeFrom: {
+          validators: {
+            notEmpty: {
+              message: 'This is field is required',
+            },
+          },
+        },
+        aePriority: {
+          validators: {
+            notEmpty: {
+              message: 'This is field is required',
+            },
+          },
+        },
+        /*               aeMessage: {
+                        validators: {
+                          notEmpty: {
+                            message: 'This is field is required',
+                          },
+                        },
+                      }, */
+
+
+      },
+
+      plugins: {
+        //Learn more: https://formvalidation.io/guide/plugins
+        trigger: new FormValidation.plugins.Trigger(),
+        // Bootstrap Framework Integration
+        bootstrap: new FormValidation.plugins.Bootstrap(),
+        // Validate fields when clicking the Submit button
+        submitButton: new FormValidation.plugins.SubmitButton(),
+        // Submit the form when all fields are valid
+        //defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+      },
+    })
+      .on('core.form.valid', function () {
+        // Show loading state on button
+        KTUtil.btnWait(formSubmitButton, _buttonSpinnerClasses, 'Please wait');
+        console.log(`Value:${aeTitle.value}`);
+
+
+        // Accessing Restful API
+        axios({
+          method: 'post',
+          url: `${HOST_URL}/api/v1/announcement/entries`,
+          data: {
+            title: aeTitle.value,
+            //message: aeMessage.value,
+            from: aeFrom.value,
+            isEmailReq: true,   //if(aeIsEmailReq.value=== 'on') return true
+            priority: (aePriority.value) * 1,
+            expires: aeExpires.value,
+          },
+
+        }).then(function (res) {
+          // Return valid JSON
+          // Release button
+          KTUtil.btnRelease(formSubmitButton);
+          console.log(res);
+
+          // SWAL EXAMPLE
+          /*         if (res.data.status == 'success') {
+              Swal.fire({
+                title: 'Success',
+                text: res.data.message,
+                icon: 'success',
+                buttonsStyling: false,
+                showCancelButton: true,
+                confirmButtonText: 'OK',
+                cancelButtonText : 'Cancel',
+                customClass: {
+                  confirmButton: 'btn font-weight-bold btn-light-primary',
+                  cancelButton : 'btn font-weight-bold btn-light-primary',
+                },
+              }).then(function (result) {
+                console.log(result);
+                if (result.value) {
                   Swal.fire({
                     title: 'Success',
-                    text: res.data.message,
                     icon: 'success',
-                    buttonsStyling: false,
-                    showCancelButton: true,
+                    text: `Your file has been deleted`,
                     confirmButtonText: 'OK',
-                    cancelButtonText : 'Cancel',
-                    customClass: {
-                      confirmButton: 'btn font-weight-bold btn-light-primary',
-                      cancelButton : 'btn font-weight-bold btn-light-primary',
-                    },
-                  }).then(function (result) {
-                    console.log(result);
-                    if (result.value) {
-                      Swal.fire({
-                        title: 'Success',
-                        icon: 'success',
-                        text: `Your file has been deleted`,
-                        confirmButtonText: 'OK',
-                      })
-                    } else if (result.isDismissed === true) {
-        
-                      Swal.fire({
-                        title: 'Cancelled',
-                        icon: 'error',
-                        text: `Your imaginary file is safe :)`,
-                        confirmButtonText: 'OK',
-                      })
-                    }
-        
                   })
-                } */
-      
-              // TOASTR EXAMPLE
-      
-              toastr.options = {
-                "closeButton": false,
-                "debug": false,
-                "newestOnTop": true,
-                "progressBar": false,
-                "positionClass": "toast-top-right",
-                "preventDuplicates": false,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "3000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-              };
-      
-              if (res.data.status == 'success') {
-                toastr.success(`${res.data.message}`, `${res.data.status}`)
-              } else if (res.data.status == 'error') {
-                toastr.error(`${res.data.message}`, `${res.data.status}`)
-              }
-            });
-      
-            })
-            .on('core.form.invalid', function () { 
-              console.log('Something went wrong!!');
-      
-            });
-            
-          };
+                } else if (result.isDismissed === true) {
     
-        
-          return {
-            // public functions
-            init: function () {      
-                _createAnnouncementEntriesForm();
-            },
+                  Swal.fire({
+                    title: 'Cancelled',
+                    icon: 'error',
+                    text: `Your imaginary file is safe :)`,
+                    confirmButtonText: 'OK',
+                  })
+                }
+    
+              })
+            } */
+
+          // TOASTR EXAMPLE
+
+          toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": true,
+            "progressBar": false,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "3000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
           };
+
+          if (res.data.status == 'success') {
+            toastr.success(`${res.data.message}`, `${res.data.status}`)
+          } else if (res.data.status == 'error') {
+            toastr.error(`${res.data.message}`, `${res.data.status}`)
+          }
+        });
+
+      })
+      .on('core.form.invalid', function () {
+        console.log('Something went wrong!!');
+
+      });
+
+  };
+
+
+  return {
+    // public functions
+    init: function () {
+      _createAnnouncementEntriesForm();
+    },
+  };
 })();
-        
+
 jQuery(document).ready(function () {
   AnnouncementCRUD.init();
-  });
+});
---
+++ assets/js/pages/crud/myForm/newsletter/newsletterMessages.js
@@ -0,0 +1,145 @@
+/* eslint-disable */
+'use strict';
+
+// Class definition
+const NewsletterCRUD = (function () {
+    const _buttonSpinnerClasses = 'spinner spinner-right spinner-white pr-15';
+
+    // Private functions
+    const _createNewsletterMessagesForm = function () {
+
+        // Getting Document related information
+        const createNewsletterMessagesForm = KTUtil.getById('createNewsletterMessagesForm');
+        const formSubmitButton = KTUtil.getById('newsletterMessagesFormSubmit');
+        const nmID = KTUtil.getById('nmID');
+        const nmSubject = KTUtil.getById('nmSubject');
+        const nmMessage = KTUtil.getById('nmMessage');
+        const nmEmail = KTUtil.getById('nmEmail');
+        const nmSent = KTUtil.getById('nmSent');
+        const nmVisited = KTUtil.getById('nmVisited');
+        const nmLastVisited = KTUtil.getById('nmLastVisited');
+
+        // Initializing 
+        $(nmMessage).summernote({
+            height: 400,
+            tabsize: 2
+        });
+        // Date & Time : Date
+        $(nmLastVisited).datepicker({
+            placeholder: "Select Date"
+        });
+
+
+
+
+        if (!createNewsletterMessagesForm) {
+            return;
+        }
+
+        FormValidation.formValidation(createNewsletterMessagesForm, {
+            fields: {
+                nmID: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Newsletter ID is required',
+                        },
+                    },
+                },
+                nmSubject: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Subject is required',
+                        },
+                    },
+                },
+                nmEmail: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Recipient Email is required',
+                        },
+                    },
+                },
+                nmMessage: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Message cannot be empty',
+                        },
+                    },
+                },
+            },
+
+            plugins: {
+                //Learn more: https://formvalidation.io/guide/plugins
+                trigger: new FormValidation.plugins.Trigger(),
+                // Bootstrap Framework Integration
+                bootstrap: new FormValidation.plugins.Bootstrap(),
+                // Validate fields when clicking the Submit button
+                submitButton: new FormValidation.plugins.SubmitButton(),
+                // Submit the form when all fields are valid
+                //defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+            },
+        })
+            .on('core.form.valid', function () {
+                KTUtil.btnWait(formSubmitButton, _buttonSpinnerClasses, 'Please wait');
+                console.log(nmID.value);
+
+                // Accessing Restful API
+                axios({
+                    method: 'post',
+                    url: `${HOST_URL}/api/v1/newsletter/messages`,
+                    data: {
+                        subject: nmSubject.value,
+                        //message: nmMessage.value,
+                        recipientEmail: nmEmail.value,
+                        sent: true,
+                        visited: true,
+                        lastVisited: nmLastVisited.value,
+                    },
+                }).then(function (res) {
+                    KTUtil.btnRelease(formSubmitButton);
+                    console.log(res);
+
+                    // TOASTR EXAMPLE
+                    toastr.options = {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": true,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "300",
+                        "hideDuration": "1000",
+                        "timeOut": "3000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    };
+
+                    if (res.data.status == 'success') {
+                        toastr.success(`${res.data.message}`, `${res.data.status}`)
+                    } else if (res.data.status == 'error') {
+                        toastr.error(`${res.data.message}`, `${res.data.status}`)
+                    }
+                });
+            })
+            .on('core.form.invalid', function () {
+                console.log('Something went wrong!!');
+            });
+    };
+
+
+    return {
+        // public functions
+        init: function () {
+            _createNewsletterMessagesForm();
+        },
+    };
+})();
+
+
+jQuery(document).ready(function () {
+    NewsletterCRUD.init();
+});
