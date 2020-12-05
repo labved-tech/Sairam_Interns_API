07809e88b1e015afe7c23cfba49f02cd69ca0774
labved <bharath.viswanathan@labved.com>
2020-12-05 12:37:14 +0530

    updated PackingList

--- models/sales-finance/packingListModel.js
+++ models/sales-finance/packingListModel.js
@@ -5,57 +5,55 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 /* SCHEMA */
-const packingListSchema = new Schema({
-
-  packingListNumber: { type: Number },
-  taxInvoiceNumber: { type: Number },
-  source: { type: String },
-  sourceAddress: { type: Object },
-  sourceEmail: { type: String },
-  sourceContactNumber: { type: String },
-  sourceGSTIN: { type: Number },
-  consignee: { type: String },
-  consigneeAddress: { type: Object },
-  consigneeEmail: { type: String },
-  consigneeContactNumber: { type: String },
-  consigneeGSTIN: { type: String },
-  box: [
-    {
-      boxNumber: { type: Number },
-      dimensions: 
-        {
+const packingListSchema = new Schema(
+  {
+    packingListNumber: { type: Number },
+    taxInvoiceNumber: { type: Number },
+    source: { type: String },
+    sourceAddress: { type: Object },
+    sourceEmail: { type: String },
+    sourceContactNumber: { type: String },
+    sourceGSTIN: { type: Number },
+    consignee: { type: String },
+    consigneeAddress: { type: Object },
+    consigneeEmail: { type: String },
+    consigneeContactNumber: { type: String },
+    consigneeGSTIN: { type: String },
+    box: [
+      {
+        boxNumber: { type: Number },
+        dimensions: {
           length: { type: Number },
           breadth: { type: Number },
           width: { type: Number },
-          units: { type: Number },
+          units: { type: String },
         },
-      
 
-      weight: { type: Number },
-      weightUnit: { type: String },
-      items: [
-        {
-          _serialNo: { type: String },
-          itemCode: { type: String },
-          name: { type: String },
-          quantity: { type: Number },
-          unitOfMeasurement: { type: String },
-          boxmeta: { type: mongoose.ObjectId },
-          createdBy: { type: mongoose.ObjectId },
-          updatedBy: { type: mongoose.ObjectId }
-        },
-      ],
-    },
-  ],
+        weight: { type: Number },
+        weightUnit: { type: String },
+        items: [
+          {
+            _serialNo: { type: String },
+            itemCode: { type: String },
+            name: { type: String },
+            quantity: { type: Number },
+            unitOfMeasurement: { type: String },
+            boxmeta: { type: mongoose.ObjectId },
+            createdBy: { type: mongoose.ObjectId },
+            updatedBy: { type: mongoose.ObjectId },
+          },
+        ],
+      },
+    ],
 
-  shipMethod: { type: String },
-  carrierName: { type: String },
-  carrierTrackingNumber: { type: Number },
-  shippingNotes: { type: String },
-  fileProof: { type: String },
-  createdBy: { type: mongoose.ObjectId },
-  updatedBy: { type: mongoose.ObjectId },
-},
+    shipMethod: { type: String },
+    carrierName: { type: String },
+    carrierTrackingNumber: { type: Number },
+    shippingNotes: { type: String },
+    fileProof: { type: String },
+    createdBy: { type: mongoose.ObjectId },
+    updatedBy: { type: mongoose.ObjectId },
+  },
   { timestamps: true }
 );
 
