fcaa6f0c55670111e96f6f6f0c60f858803e19b6
labved <bharath.viswanathan@labved.com>
2020-12-08 16:09:15 +0530

    Update menuAside.js

--- assets/js/menuAside.js
+++ assets/js/menuAside.js
@@ -26,13 +26,13 @@ const MenuFetch = (function () {
       console.log('sectionLen', sectionLen);
       for (let i = 0; i < sectionLen; i++) {
         $('#menuList').append('\
-        <!-- Begin:: Section '+ asideMenu.manager[0]._section[i].name +' -->  \
+        <!-- Begin::Section - '+ asideMenu.manager[0]._section[i].name +' -->  \
         \
         <li id="'+ asideMenu.manager[0]._section[i].name +'"class="menu-section ">\
           <h4 class="menu-text">'+ asideMenu.manager[0]._section[i].name +'</h4>\
           <i class="menu-icon ki ki-bold-more-hor icon-md"></i>\
         </li>\
-        <!-- End:: Section:'+ asideMenu.manager[0]._section[i].name +'-->  \
+        <!-- End::Section: - '+ asideMenu.manager[0]._section[i].name +'-->  \
         ')
         
         // Constructing Item
@@ -42,14 +42,15 @@ const MenuFetch = (function () {
         if (itemLen > 0) {
           for (let j = 0; j < itemLen; j++) {
             $('#menuList').append('\
-              <!-- Begin:: Item :'+ asideMenu.manager[0]._section[i]._item[j].name +' -->  \
-              <li id="'+asideMenu.manager[0]._section[i]._item[j].name+'List" class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">\
-                  <a href="javascript:;" class="menu-link menu-toggle">\
+              <!-- Begin:: li Item :'+ asideMenu.manager[0]._section[i]._item[j].name +' -->  \
+              <li id="li'+ asideMenu.manager[0]._section[i]._item[j].name +'" class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">\
+                  <a href="'+ asideMenu.manager[0]._section[i]._item[j].route + '" class="menu-link menu-toggle">\
                       <i class="menu-bullet menu-bullet-dot">\
                           <span></span>\
                       </i>\
-                      <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j].name +'</span><i class="menu-arrow"></i></a>\
-                  <div class="menu-submenu "><i class="menu-arrow"></i>\
+                      <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j].name + '</span> \
+                      <i id="i'+ asideMenu.manager[0]._section[i]._item[j].name +'" class= "menu-arrow" ></i >\
+                  </a>\
             ');
             
             // Constructing SubItem 1
@@ -59,12 +60,13 @@ const MenuFetch = (function () {
             if (subItem1Len > 0) {
               for (let k = 0; k < subItem1Len; k++) {
 
-                // Adding Parent Item
+                // Adding Parent Item for Item
                 if (k == 0) {
-                  $('#'+asideMenu.manager[0]._section[i]._item[j].name+'List').append('\
+                  $('#li'+ asideMenu.manager[0]._section[i]._item[j].name).append('\
                   <div class="menu-submenu ">\
-                  <i class="menu-arrow"></i>\
-                    <ul id= "'+asideMenu.manager[0]._section[i]._item[j].name+'uList" class="menu-subnav">\
+                    <i class="menu-arrow"></i>\
+                    <!-- Begin:: uL Item :'+ asideMenu.manager[0]._section[i]._item[j].name +' -->  \
+                    <ul id= "ul'+ asideMenu.manager[0]._section[i]._item[j].name +'" class="menu-subnav">\
                         <li class="menu-item  menu-item-parent" aria-haspopup="true">\
                             <span class="menu-link">\
                                 <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j].name +'</span>\
@@ -73,16 +75,17 @@ const MenuFetch = (function () {
                 ');
                 }
 
-                // Adding Sub Item1
-                $('#'+asideMenu.manager[0]._section[i]._item[j].name+'uList').append('\
-                <!-- Begin:: subItem1 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +' -->  \
-                <li id="'+asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name+'List" class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover"> \
-                  <a href="javascript:;" class="menu-link menu-toggle"> \
+                // Adding li subItem1
+                $('#ul'+ asideMenu.manager[0]._section[i]._item[j].name).append('\
+                <!-- Begin:: li subItem1 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +' -->  \
+                <li id="li'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +'" class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover"> \
+                  <a href="'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].route + '" class="menu-link menu-toggle"> \
                       <i class="menu-bullet menu-bullet-dot"> \
                           <span></span> \
                       </i> \
                       <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +'</span> \
-                      <i class="menu-arrow"></i></a> \
+                      <i id="'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +'" class="menu-arrow"></i>\
+                  </a > \
               '); 
                 
                 
@@ -91,65 +94,56 @@ const MenuFetch = (function () {
                 console.log('subItem1Len', subItem1Len);
                 
                 if (subItem2Len > 0) {
-                  for (let l = 0; l < subItem2Len; l++) {
-                    // Adding Parent Item
-                    if (l == 0) {
-                      $('#'+asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name+'List').append('\
-                      <div class="menu-submenu ">\
-                      <i class="menu-arrow"></i>\
-                        <ul id="'+asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name+'uList" class="menu-subnav">\
-                            <li class="menu-item  menu-item-parent" aria-haspopup="true">\
-                                <span class="menu-link">\
-                                    <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j].name +'</span>\
-                                </span>\
-                            </li>\
-                    ');
-                    }
 
-                    // Adding Sub Item2
-                    $('#' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + 'uList').append('\
-                    <!-- Begin:: subItem2 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + ' -->  \
-                    <li class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover"> \
-                      <a href="javascript:;" class="menu-link menu-toggle"> \
+                  // Opening ul subItem 1
+                  $('#li' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).append('\
+                  <div class="menu-submenu ">\
+                    <i class= "menu-arrow" ></i >\
+                    <!-- Begin:: ul subItem1 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name + ' -->  \
+                    <ul id="ul'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +'" class="menu-subnav">\
+                  ');
+
+                  for (let l = 0; l < subItem2Len; l++) {
+                    // Adding li subItem2
+                    $('#ul' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).append('\
+                    <!-- Begin:: li subItem2 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + ' -->  \
+                    <li class="menu-item" aria-haspopup="true"> \
+                      <a href="'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].route + '" class="menu-link menu-toggle"> \
                           <i class="menu-bullet menu-bullet-dot"> \
                               <span></span> \
                           </i> \
                           <span class="menu-text">'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + '</span> \
-                          <i class="menu-arrow"></i></a> \
                     </li>\
-                    <!-- End:: subItem2:'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + ' -->  \
+                    <!-- End:: li subItem2:'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[l].name + ' -->  \
                       ');
-                    
                   }
-                  // Closing Sub Item 2 Unordered List
-                  $('#'+asideMenu.manager[0]._section[i]._item[j]._subItem1[k]._subItem2[subItem2Len-1].name+'uList').append('\
-                  </ul> \
-                  '); 
+                  // Closing ul subItem 1
+                  $('#ul' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).append('\
+                  <!-- End:: ul subItem1 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name + ' -->  \
+                  </ul>');
                 }
-                
-                // Closing Sub Item 1 List  
-                $('#' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name + 'List').append('\
-                </li>\
-                <!-- End:: subItem1 '+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name + ' -->  \
-                '); 
-              }
-              // Closing Sub Item 1 Unorder List
-              $('#' + asideMenu.manager[0]._section[i]._item[j]._subItem1[subItem1Len-1].name + 'uList').append('\
-              </ul>\
-              ');
+                else {
+                  $('#li' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).removeAttr("data-menu-toggle");
+                  $('#i'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).remove();
+                }
+                // Closing li subItem 1
+                $('#li' + asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name).append('\
+                  <\li>\
+                  <!-- End:: li subItem1 :'+ asideMenu.manager[0]._section[i]._item[j]._subItem1[k].name +' -->  \
+                ')
+                // Closing ul Item
+                $('#ul' + asideMenu.manager[0]._section[i]._item[j].name).append('\
+                  </ul>\
+                  <!-- End:: uL Item :'+ asideMenu.manager[0]._section[i]._item[j].name +' -->  \
+                ')
+             }
             }
-
-            // Closing Item List
-            $('#'+asideMenu.manager[0]._section[i]._item[j].name+'List').append('\
-              </li>\
-              <!-- End:: Item '+ asideMenu.manager[0]._section[i]._item[j].name +'-->  \
-            ')
+            else {
+              console.log($('#li' + asideMenu.manager[0]._section[i]._item[j].name).html());
+              $('#li'+ asideMenu.manager[0]._section[i]._item[j].name).removeAttr("data-menu-toggle");
+              $('#i'+ asideMenu.manager[0]._section[i]._item[j].name).remove();
+            }
           }
-
-        // Closing Item Unorder List
-        $('#'+asideMenu.manager[0]._section[i]._item[itemLen-1].name+'uList').append('\
-        </ul>\
-        ');
         }
       }
       $('#menuList').append('</ul>');
