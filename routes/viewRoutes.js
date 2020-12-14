d1f254d5024df753b589bcef8e4f8bb588e56f8a
labved <bharath.viswanathan@labved.com>
2020-12-14 16:47:15 +0530

    changed directory module

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -60,7 +60,10 @@ router.get(
   '/ratings/attribute/groups/table',
   viewsController.ratingAttributeGroupsTable
 );
-router.get('/ratings/attribute/groups/table', viewsController.ratingAttributeGroupsTable);
+router.get(
+  '/ratings/attribute/groups/table',
+  viewsController.ratingAttributeGroupsTable
+);
 router.get('/ratings/entries/table', viewsController.ratingEntriesTable);
 
 // ANALYTICS RELATED ROUTES
@@ -88,11 +91,20 @@ router.get('/sales-finance/quotation', viewsController.quotation);
 router.get('/sales-finance/taxInvoice', viewsController.taxInvoice);
 router.get('/sales-finance/performaInvoice', viewsController.performaInvoice);
 router.get('/sales-finance/address/table', viewsController.addressTable);
-router.get('/sales-finance/deliveryNote/table', viewsController.deliveryNoteTable);
-router.get('/sales-finance/packingList/table', viewsController.packingListTable);
+router.get(
+  '/sales-finance/deliveryNote/table',
+  viewsController.deliveryNoteTable
+);
+router.get(
+  '/sales-finance/packingList/table',
+  viewsController.packingListTable
+);
 router.get('/sales-finance/quotation/table', viewsController.quotationTable);
 router.get('/sales-finance/taxInvoice/table', viewsController.taxInvoiceTable);
-router.get('/sales-finance/performaInvoice/table', viewsController.performaInvoiceTable);
+router.get(
+  '/sales-finance/performaInvoice/table',
+  viewsController.performaInvoiceTable
+);
 
 // EVENT RELATED ROUTES
 router.get('/event/entries', viewsController.eventEntries);
@@ -129,16 +141,14 @@ router.get('/directory/categories', viewsController.directoryCategories);
 router.get('/directory/levels', viewsController.directoryLevels);
 router.get('/directory/entries', viewsController.directoryEntries);
 router.get('/directory', viewsController.directory);
-// router.get(
-//   '/directory/categories/table',
-//   viewsController.ratingCategoriesTable
-// );
-// router.get(
-//   '/directory/attributes/groups/table',
-//   viewsController.directoryAttributesGroupsTable
-// );
-router.get('/directory/categories/table', viewsController.directoryCategoriesTable);
-router.get('/directory/attributes/groups/table', viewsController.directoryAttributesGroupsTable);
+router.get(
+  '/directory/categories/table',
+  viewsController.directoryCategoriesTable
+);
+router.get(
+  '/directory/attributes/groups/table',
+  viewsController.directoryAttributeGroupsTable
+);
 router.get('/directory/levels/table', viewsController.directoryLevelsTable);
 router.get('/directory/entries/table', viewsController.directoryEntriesTable);
 router.get('/directory/table', viewsController.directoryTable);
