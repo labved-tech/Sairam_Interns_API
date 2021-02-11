f0d27264983d501e0f29a88de065083358d242ea
labved <bharath.viswanathan@labved.com>
2021-02-11 16:22:20 +0530

    updated sales-finance quotation

--- models/sales-finance/quotationModel.js
+++ models/sales-finance/quotationModel.js
@@ -5,41 +5,40 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 /* SCHEMA */
-const quotationSchema = new Schema({
- 
-  moduleReference: { type: String },
-  header: { type: String },
-  date: { type: Date },
-  _buyerId: { type: mongoose.ObjectId },
-  buyerName: { type: String },
-  buyerAddress: { type: Object },
-  buyerEmail: { type: String },
-  buyerContactNumber: { type: Number },
-  buyerContactNumbertype: { type: String },
-  _sellerId: { type: mongoose.ObjectId },
-  sellerName: { type: String },
-  sellerAttentionName: { type: String },
-  sellerAddress: { type: Object },
-  sellerEmail: { type: String },
-  sellerContactNumber: { type: Number },
-  sellerContactNumbertype: { type: String },
-  subject: { type: String },
-  body: { type: String },
-  itemTable: [],
-  termsAndConditions: { type: String },
-  footer: { type: String },
-  paymentMethods: {
-    _id: { type: mongoose.ObjectId },
-    type: { type: String },
-    meta: { type: Object },
+const quotationSchema = new Schema(
+  {
+    moduleReference: { type: String },
+    header: { type: String },
+    date: { type: Date },
+    _buyerId: { type: mongoose.ObjectId },
+    buyerName: { type: String },
+    buyerAddress: { type: Object },
+    buyerEmail: { type: String },
+    buyerContactNumber: { type: Number },
+    buyerContactNumbertype: { type: String },
+    _sellerId: { type: mongoose.ObjectId },
+    sellerName: { type: String },
+    sellerAttentionName: { type: String },
+    sellerAddress: { type: Object },
+    sellerEmail: { type: String },
+    sellerContactNumber: { type: Number },
+    sellerContactNumbertype: { type: String },
+    subject: { type: String },
+    body: { type: String },
+    itemTable: [],
+    termsAndConditions: { type: String },
+    footer: { type: String },
+    paymentMethods: {
+      _id: { type: mongoose.ObjectId },
+      type: { type: String },
+      meta: { type: Object },
+    },
+    quotationNumber: { type: Number },
+    createdBy: { type: mongoose.ObjectId },
+    updatedBy: { type: mongoose.ObjectId },
   },
-  quotationNumber: { type: Number },
-  createdBy: { type: mongoose.ObjectId  },
-  updatedBy: { type: mongoose.ObjectId },
-},
-{timestamps: true}
+  { timestamps: true }
 );
-
 
 /* MODEL */
 const Quotation = mongoose.model('quotation', quotationSchema);
